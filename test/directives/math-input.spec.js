6dcfe216516ce649a9e97fa1b5689402dfdd8781
Attila Miklosi <attila.miklosi@gmail.com>
2016-01-28 20:17:43 +0100

    tests wip

--- test/directives/math-input.spec.js
+++ test/directives/math-input.spec.js
@@ -8,6 +8,7 @@ describe('math input', function() {
     beforeEach(function() {
       $.fn.extend({mathquill: function() {
       }});
+      spyOn($.fn, "mathquill").and.returnValue(function() {});
     });
 
     function createDirective(opts) {
@@ -15,7 +16,9 @@ describe('math input', function() {
       return inject(function($compile, $rootScope) {
         scope = $rootScope.$new();
         var node = angular.element([
-          '<math-input editable="' + opts.editable + '">',
+          '<math-input editable="' + opts.editable + '"',
+          opts.expression ? ('expression="\''+opts.expression+'\'"') : '',
+          '>',
           '</math-input>'
         ].join('\n'));
         element = $compile(node)(scope);
@@ -24,10 +27,33 @@ describe('math input', function() {
     }
 
     describe('init', function() {
-      beforeEach(createDirective());
       it('keypad is hidden, no input focused', function() {
+        createDirective();
         expect(directiveScope.showKeypad).toBe(false);
         expect(directiveScope.focusedInput).toBe(null);
+      });
+      it('editable by default', function() {
+        createDirective();
+        expect($.fn.mathquill.calls.argsFor(0)).toEqual(['editable']);
+      });
+      it('calls mathquill with non editable if editable is false', function() {
+        createDirective({editable: false});
+        expect($.fn.mathquill.calls.argsFor(0)).toEqual([undefined]);
+      });
+      it('without expression', function() {
+        createDirective();
+        expect($.fn.mathquill.calls.count()).toEqual(1);
+        expect($.fn.mathquill.calls.argsFor(0)).toEqual(['editable']);
+      });
+      it('with expression', function() {
+        createDirective({expression: "2x+3"});
+        expect($.fn.mathquill.calls.count()).toEqual(2);
+        expect($.fn.mathquill.calls.argsFor(0)).toEqual(['editable']);
+        expect($.fn.mathquill.calls.argsFor(1)).toEqual(['latex','2x+3']);
+      });
+      it('expression populates ngModel', function() {
+        createDirective({expression: '\\\\frac{3}{4}'});
+        expect(directiveScope.ngModel).toEqual('\\\\frac{3}{4}');
       });
     });
   });
